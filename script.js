5008894a68781d469a5b8bd115f453285e61f832
alvincasilac <casilac546@gmail.com>
2026-07-08 01:40:19 +0800

    Add Google Analytics tracking for button clicks

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function nextPage() {
+    // Track yes button click
+    if (typeof gtag !== 'undefined') {
+        gtag('event', 'click', {
+            'event_category': 'button',
+            'event_label': 'yes'
+        });
+    }
     window.location.href = "yes.html";
 }
 
 function moveButton() {
+    // Track no button click/hover
+    if (typeof gtag !== 'undefined') {
+        gtag('event', 'click', {
+            'event_category': 'button',
+            'event_label': 'no'
+        });
+    }
+    
     let button = document.getElementById("noButton");
     let maxX = window.innerWidth - button.offsetWidth;
     let maxY = window.innerHeight - button.offsetHeight;
